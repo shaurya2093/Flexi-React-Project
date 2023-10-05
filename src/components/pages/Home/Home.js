e2ad97fa74c2d5c886b500c4f8abdca1587c88bd
Shaurya Gupta <111460487+shaurya2093@users.noreply.github.com>
2023-10-05 10:09:21 +0530

    Update Home.js

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,17 +1,25 @@
+// Importing React library for creating React components
 import React from 'react';
-import '../../../App.css';
-import Cards from '../../Cards';
-import HeroSection from '../../HeroSection';
-import Footer from '../../Footer';
 
+// Importing the CSS file for styling
+import '../../../App.css';
+
+// Importing child components
+import Cards from '../../Cards';          // Importing the Cards component
+import HeroSection from '../../HeroSection';  // Importing the HeroSection component
+import Footer from '../../Footer';        // Importing the Footer component
+
+// Functional component for the Home page
 function Home() {
+  // Rendering the Home page components
   return (
     <>
-      <HeroSection />
-      <Cards />
-      <Footer />
+      <HeroSection />   {/* Rendering the HeroSection component */}
+      <Cards />         {/* Rendering the Cards component */}
+      <Footer />        {/* Rendering the Footer component */}
     </>
   );
 }
 
+// Exporting the Home component as the default export for this file
 export default Home;
